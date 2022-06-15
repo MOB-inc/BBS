b565b271635dab45f372b7404ec387b1e944d6d9
Youhei Aikou <mobinc.yaikou@gmail.com>
2022-06-15 15:52:25 +0900

    inquirry-page

--- src/modules/common/inquiry/Inquiry.jsx
+++ src/modules/common/inquiry/Inquiry.jsx
@@ -1,18 +1,64 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
+
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import InputBase from '@material-ui/core/InputBase';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import MuiButton from '@material-ui/core/Button';
+
 import useFetch from 'use-http';
 import { useMount } from 'ahooks';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 import { LOCATIONS_LIST, INQUIRY } from '../../../commons/constants/url';
-import Button from '../../../commons/components/Button';
 import { errorMessageGenerator } from '../../../commons/helpers/validation';
 import Explanation from '../../../commons/components/Explanation';
 import config from '../../../OEMConfig';
+import { ReactComponent as SIcon } from '../../../commons/icons/sendArrow.svg';
 
 import './inquiry.scss';
 
+
+
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}))(InputBase);
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
 function Inquiry() {
+	const classes = useStyles();
+  const [age, setAge] = React.useState('');
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   const { t } = useTranslation();
   const history = useHistory();
   const [locations, setLocations] = useState([]);
@@ -54,40 +100,53 @@ function Inquiry() {
         <>
           <div className="input">
             <Explanation screen="INQUIRY" />
-            <div>{t('common:INQUIRY:LOCATION')}</div>
-            <select
-              onChange={(event) => setLocationId(event.target.value)}
-              value={locationId}
-            >
-              <option value={0}>Select</option>
-              {locations.map((l) => {
-                return (
-                  <option key={l.id} value={l.id}>
-                    {l.name?.length > 15 ? `${l.name.slice(0, 15)}...` : l.name}
-                  </option>
-                );
-              })}
-            </select>
+            <FormControl className={classes.margin}>
+              <InputLabel>
+                {t('common:INQUIRY:LOCATION')}
+              </InputLabel>
+              <NativeSelect
+                input={<BootstrapInput />}
+                onChange={(event) => setLocationId(event.target.value)}
+                value={locationId}
+              >
+                <option value={0}>Location</option>
+                {locations.map((l) => {
+                  return (
+                    <option key={l.id} value={l.id}>
+                      {l.name?.length > 15
+                        ? `${l.name.slice(0, 15)}...`
+                        : l.name}
+                    </option>
+                  );
+                })}
+              </NativeSelect>
+            </FormControl>
           </div>
-          <br />
-          <br />
           <div className="input">
             <div>{t('common:INQUIRY:INQUIRY')}</div>
-            <textarea
-              className="textarea"
-              name=""
-              rows={5}
-              onChange={(event) => setContents(event.target.value)}
+            <TextareaAutosize
+              aria-label="minimum height"
+              // minRows={3}
+              placeholder="Minimum 3 rows"
             />
           </div>
-          <br /> <br />
-          <Button onClick={sendHandler} disabled={!(contents?.length > 0)}>
-            {t('common:INQUIRY:SEND')}
-          </Button>
-          <br />
-          <Button type="reset" onClick={() => history.goBack()}>
-            {t('common:INQUIRY:RETURN')}
-          </Button>
+          <MuiButton
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            endIcon={<SIcon />}
+            onClick={sendHandler}
+            // disabled={!(contents?.length > 0)}
+          >
+            送る
+          </MuiButton>
+          <MuiButton
+            variant="outlined"
+            color="secondary"
+            onClick={() => history.goBack()}
+          >
+            戻る
+          </MuiButton>
           {config().inquiries_phone_number && (
             <div className="contact">
               <p className="phone-message">
--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,7 @@ const Locations = React.lazy(() => import('./modules/location/location'));
 const BasicInfoTab = React.lazy(() =>
   import('./modules/basicInfo/Navigation/TabInfo'),
 );
+const Aikou = React.lazy(() => import('./modules/aikou'));
 const Top = React.lazy(() => import('./modules/top/Top'));
 const Components = React.lazy(() => import('./modules/components'));
 const Users = React.lazy(() => import('./modules/users'));
@@ -27,6 +28,12 @@ const Notification = React.lazy(() =>
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
+  {
+    path: '/aikou',
+    name: 'Aikou',
+    component: Aikou,
+    isPrivate: true,
+  },
   {
     path: '/top',
     name: 'Top',
